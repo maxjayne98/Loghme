6f428689b9172be3cd29bf0f7e691b6d62b3de9b
mahdi <mahdi.jax@gmail.com>
2020-02-08 18:45:33 +0330

    enhanced loghme class

--- src/main/java/Loghme.java
+++ src/main/java/Loghme.java
@@ -5,35 +5,39 @@ import com.google.gson.GsonBuilder;
 public class Loghme {
     private HashMap<String, Restaurant> Restaurants = new HashMap<String, Restaurant>();
     static Scanner input = new Scanner(System.in);
-    GsonBuilder builder = new GsonBuilder();
+
 
     public void CommandHandler(){
 
         while (true) {
             String myString = input.nextLine();
-            String[] words = myString.split(" ", 1);
+            String[] words = myString.split(" ", 2);
 
             switch (words[0]){
                 case "addRestaurant":
-                    addRestaurant();
+                    addRestaurant(words[1]);
                     break;
                 case "addFood":
                     System.out.println("salam2");
                     break;
-                case "getRestraunts":
+                case "getRestaurants":
                     getRestraunts();
                     break;
                 default:
                     System.out.println("default");
             }
 
-
         }
     }
-    private void addRestaurant(){
-        Restaurant newRestaurant = new Restaurant("salam","lux",1,2);
-        System.out.println(newRestaurant.getName());
-        Restaurants.put("salam",newRestaurant);
+    private void addRestaurant(String jsonString){
+        Gson gson = new Gson();
+        System.out.println("your input :" + jsonString);
+        Restaurant newRestaurant = gson.fromJson(jsonString, Restaurant.class);
+        //for input testing
+        String jsonnString = gson.toJson(newRestaurant);
+        System.out.println(jsonnString);
+
+        Restaurants.put(newRestaurant.getName(),newRestaurant);
 
     }
     public HashMap<String, Restaurant> getRestaurants() {
