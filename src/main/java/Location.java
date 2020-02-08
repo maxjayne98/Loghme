a793e33ec0e8b2471ec5281d631a9a4428e2d04b
mahdi <mahdi.jax@gmail.com>
2020-02-08 18:44:57 +0330

    add location class for restaurant

---
+++ src/main/java/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    private Integer x;
+    private Integer y;
+
+    public Location(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getXLocation(){
+        return this.x ;
+    }
+    public Integer getYLocation(){
+        return this.x ;
+    }
+}
