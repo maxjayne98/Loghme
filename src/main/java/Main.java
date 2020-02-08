610e6bedef40e7e064881bdabedb8e44d54094ee
mahdi <mahdi.jax@gmail.com>
2020-02-08 15:19:25 +0330

    initial loghme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("salam");
+        Loghme loghme = new Loghme();
+        loghme.CommandHandler();
     }
 
 }
