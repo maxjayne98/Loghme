4886493010be1af71233e2a1a67b4dd86655fe8c
mahdi <mahdi.jax@gmail.com>
2020-02-08 21:20:41 +0330

    add cart class

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Cart {
+    private Integer total;
+    private String RestaurantName;
+    private List<Food> selectedFoods;
+    Cart(){
+
+    }
+    public void addFood(){
+
+    }
+    public List<Food> getSelectedFoods() {
+        return selectedFoods;
+    }
+
+    public Integer getSum() {
+        return total;
+    }
+
+    public void updateSum(Integer sum) {
+        this.total = sum;
+    }
 }
