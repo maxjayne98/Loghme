9a3aefc4396f4bbfca4d827076940aacecb60cb5
mahdi <mahdi.jax@gmail.com>
2020-02-08 19:12:38 +0330

    add addFood func for restaurant

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,3 +1,7 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.util.*;
 public class Restaurant {
 
@@ -19,6 +23,18 @@ public class Restaurant {
 
     public List<Food> getMenu() {
         return menu;
+    }
+
+    public void addFoodToMenu(String jsonString) {
+        JsonElement jsonElement = new JsonParser().parse(jsonString);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        String foodName = jsonObject.get("name").getAsString();
+        String description = jsonObject.get("description").getAsString();
+        Integer price = jsonObject.get("description").getAsInt();
+        float popularity = jsonObject.get("popularity").getAsFloat();
+        
+        Food newFood = new Food(foodName,description,popularity,price);
+        menu.add(newFood);
     }
 }
 //addRestaurant {"name": "Hesturan", "description": "luxury", "location": {"x": 1, "y": 3},"menu": [{"name": "Gheime", "description": "it’s yummy!", "popularity": 0.8, "price":20000}, {"name": "Kabab", "description": "it’s delicious!", "popularity": 0.6, "price":30000}]}
