d5c9f4c599568753bee519164e8d9ba397a6298c
mahdi <mahdi.jax@gmail.com>
2020-02-08 18:45:43 +0330

    compelete food class

--- src/main/java/Food.java
+++ src/main/java/Food.java
@@ -1,5 +1,13 @@
 public class Food {
-    Food(){
+    private String name;
+    private String description;
+    private float popularity;
+    private Integer price;
 
+    public Food(String name, String description, Integer popularity, Integer price) {
+        this.name = name;
+        this.description = description;
+        this.popularity = popularity;
+        this.price = price;
     }
 }
