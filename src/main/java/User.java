bd22df78dcb0f7e6d0b878a9cc04ca77071b3674
mahdi <mahdi.jax@gmail.com>
2020-02-09 11:16:48 +0330

    add feature to user class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,9 +1,34 @@
+import com.google.gson.Gson;
+
+import java.util.List;
+
 public class User {
     Cart cart = new Cart();
     User(){
 
     }
-    public void addToCart(){
+    public void addToCart(String jsonString, Restaurant selectedRestaurant, Food selectedFood){
+        if (cart.getRestaurantName() == null){
+            cart.setRestaurantName(selectedRestaurant.getName());
+        }
 
+        if (selectedRestaurant.getName().equals(cart.getRestaurantName())){
+            cart.addFood(selectedFood);
+        }
+        else{
+            System.out.println("your selected food can't add to your cart ...");
+        }
+    }
+    public void getCart(){
+        Gson gson = new Gson();
+        List<Food> menu = cart.getFoods();
+        String jsonnString = gson.toJson(menu);
+        System.out.println(jsonnString);
+
+    }
+
+    public void finalizeOrder() {
+        cart.setRestaurantName(null);
+        cart.updateTotal(0);
     }
 }
